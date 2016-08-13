745958dca0d17ff1b7ed5da959393ba389b34ba6
tonyspurs <t.z.chen96@gmail.com>
2016-08-12 22:26:32 -0400

    mobile/tablet responsive

--- questions/2.php
+++ questions/2.php
@@ -7,12 +7,12 @@
         <title>How Much to Make a Bot</title>
         <meta name="description" content="How Much to Make a Bot">
         <meta name="author" content="Rhodium">
-
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css">
-    <script   src="https://code.jquery.com/jquery-2.2.4.js"   integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="   crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha/css/bootstrap.min.css">
+        <script   src="https://code.jquery.com/jquery-2.2.4.js"   integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="   crossorigin="anonymous"></script>
 
         <link rel="stylesheet" href="../css/styles.css">
-        	<link rel="stylesheet" type="text/css" href="../css/common.css" />
+        <link rel="stylesheet" type="text/css" href="../css/common.css" />
         <link rel="stylesheet" type="text/css" href="../css/style6.css" />
 
         <!--[if lt IE 9]>
@@ -100,7 +100,13 @@
 				</ul>
 				
 			</section>
-            
-        </div>
+                    </div>
+                        <div class="w3-progress-container">
+            <div id="myBar" class="w3-progressbar w3-dark-grey" style="width:40%">
+
+                <!-- <div class="w3-center w3-text-white">20%</div> -->
+            </div>
+            </div>
+
     </body>
 </html>
